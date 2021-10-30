05ac7cdc34afc83ff345956244bfba7823b1dca5
Paulina Luna <luna.paulina12@gmail.com>
2021-10-30 14:39:59 -0300

    Inscripciones cursos end

--- UI.Web/InscripcionesCursos.aspx
+++ UI.Web/InscripcionesCursos.aspx
@@ -2,38 +2,23 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
     <div id="container">
-        <asp:Label ID="Label1" runat="server" Text="ALTAS, BAJAS Y MODIFICACIONES DE LAS INSCRIPCIONES A CURSOS"></asp:Label>
+        <asp:Label ID="Label1" runat="server" Text="INSCRIPCIONES A CURSOS"></asp:Label>
     </div>
     
     <div id="margen"></div>
 	<section id="content">
 		<asp:Panel ID="Panel1" runat="server">
-                <asp:GridView ID="gvInscripcionesCursos" runat="server" Width="100%" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" DataKeyNames="ID" OnSelectedIndexChanged="gvInscripcionesCursos_SelectedIndexChanged" OnRowDataBound="OnRowDataBound">
+            <h3>Seleccione el curso que desea inscribirse </h3>
+                <asp:GridView ID="gvInscripcionesCursos" runat="server" Width="100%" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" DataKeyNames="ID" OnSelectedIndexChanged="gvInscripcionesCursos_SelectedIndexChanged">
                     <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
                     <Columns>
                         <asp:BoundField DataField="ID" HeaderText="ID" />
                         <asp:BoundField DataField="Descripcion" HeaderText="Curso" />
 
-                        <asp:TemplateField HeaderText="Materia">
-                            <EditItemTemplate>
-                                <asp:TextBox ID="txtDescMateria" runat="server"></asp:TextBox>
-                            </EditItemTemplate>
-                            <ItemTemplate>
-                                <asp:DropDownList ID="ddlIDMateria" runat="server" Width="179px" DataTextField="Materia" DataValueField="ID" AutoPostBack="true">
-                                </asp:DropDownList>
-                            </ItemTemplate>
-                        </asp:TemplateField>
+                        <asp:BoundField DataField="DescripcionMateria" HeaderText="Materia" />
                         <asp:BoundField DataField="AnioCalendario" HeaderText="Año Calendario"/>
                         
-                        <asp:TemplateField HeaderText="Comisión">
-                            <EditItemTemplate>
-                                <asp:TextBox ID="txtComision" runat="server"></asp:TextBox>
-                            </EditItemTemplate>
-                            <ItemTemplate>
-                                <asp:DropDownList ID="ddlIDComision" runat="server" Width="179px" DataTextField="Comisión" DataValueField="ID" AutoPostBack="true">
-                                </asp:DropDownList>
-                            </ItemTemplate>
-                        </asp:TemplateField>
+                        <asp:BoundField DataField="DescripcionComision" HeaderText="Comisión" />
 
                         <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" />
                     </Columns>
@@ -51,10 +36,8 @@
 
                 </asp:GridView>
             </asp:Panel>
-            <asp:Panel ID="Panel3" runat="server">
-                <asp:LinkButton ID="editarlinkButton" runat="server" OnClick="editarlinkButton_Click">Editar</asp:LinkButton>
-                <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
-                <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Agregar</asp:LinkButton>
+        <asp:Panel ID="Panel3" runat="server">
+                <asp:LinkButton ID="inscribirlinkButton" runat="server" OnClick="inscribirlinkButton_Click">Inscribirme</asp:LinkButton>
             </asp:Panel>
     </section>
     
@@ -65,28 +48,18 @@
 							    <td align="center" colspan="2">
 							        <strong>
                                     <asp:Label ID="lblTitulo" runat="server" Text="DATOS"></asp:Label>
-                                    </strong></td>
+                                    &nbsp;DE LA INSCRIPCION</strong></td>
 						    </tr>
 						    <tr>
-                                <!-- DESC_CURSO/ID - DESC_MATERIA/ID - DESC_COMISION/ID DropList-->
-							    <td align="right"><asp:Label ID="lblDescripcionCurso" runat="server" Text="Descripción Curso: "></asp:Label></td>
-                                <td><asp:DropDownList ID="ddlIDCurso" runat="server" Width="100%"></asp:DropDownList>
+							    <td align="right">Alumno</td>
+                                <td>
+                                    <asp:TextBox ID="txtIdAlumno" runat="server" Width="25"></asp:TextBox><asp:TextBox ID="txtNombreAlumno" runat="server"></asp:TextBox>
                                 </td>
 						    </tr>
 						    <tr>
-							    <td align="right"><asp:Label ID="lblDescripcionMateria" runat="server" Text="Descripción Materia: "></asp:Label></td>
-                                <td><asp:DropDownList ID="ddlIDMateria" runat="server" Width="100%"></asp:DropDownList>
-                                </td>
-						    </tr>
-				            <tr>
-                                <td align="right"><asp:Label ID="lblAnioCalendario" runat="server" Text="Año Calendario: "></asp:Label></td>
-                                <td><asp:TextBox ID="txtAnioCalendario" runat="server" />
-                                    <asp:RequiredFieldValidator ID="anioCalendario" runat="server" ControlToValidate="txtAnioCalendario" ErrorMessage="El Año Calendario no puede ser vacío" ForeColor="Red"  Text="*"   />
-                                </td>
-                            </tr>
-                            <tr>
-							    <td align="right"><asp:Label ID="lblDescripcionComision" runat="server" Text="Descripción Comisión: "></asp:Label></td>
-                                <td><asp:DropDownList ID="ddlIDComision" runat="server" Width="100%"></asp:DropDownList>
+							    <td align="right">Curso</td>
+                                <td>
+                                    <asp:TextBox ID="txtIdCurso" runat="server" Width="25"></asp:TextBox><asp:TextBox ID="txtDescripcionCurso" runat="server"></asp:TextBox>
                                 </td>
 						    </tr>
 						    <tr>
--- UI.Web/InscripcionesCursos.aspx.cs
+++ UI.Web/InscripcionesCursos.aspx.cs
@@ -17,25 +17,6 @@ namespace UI.Web
             if (!IsPostBack)
             {
                 this.LoadGrid();
-
-                // se cargan los controles del form
-                ComisionLogic cl = new ComisionLogic();
-                ddlIDComision.DataSource = cl.GetAll();
-                ddlIDComision.DataTextField = "Descripcion";
-                ddlIDComision.DataValueField = "ID";
-                ddlIDComision.DataBind();
-
-                CursoLogic curl = new CursoLogic();
-                ddlIDComision.DataSource = curl.GetAll();
-                ddlIDComision.DataTextField = "Descripcion";
-                ddlIDComision.DataValueField = "ID";
-                ddlIDComision.DataBind();
-
-                MateriaLogic mateL = new MateriaLogic();
-                ddlIDMateria.DataSource = mateL.GetAll();
-                ddlIDMateria.DataTextField = "Descripcion";
-                ddlIDMateria.DataValueField = "ID";
-                ddlIDMateria.DataBind();
             }
         }
 
@@ -53,55 +34,12 @@ namespace UI.Web
             }
         }
 
-        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
-        {
-            if (e.Row.RowType == DataControlRowType.DataRow)
-            {
-                DropDownList ddlIDComision = e.Row.FindControl("ddlIDComision") as DropDownList;
-                DropDownList ddlIDMateria = e.Row.FindControl("ddlIDMateria") as DropDownList;
-
-                ComisionLogic cl = new ComisionLogic();
-                ddlIDComision.DataSource = cl.GetAll();
-                ddlIDComision.DataTextField = "Descripcion";
-                ddlIDComision.DataValueField = "ID";
-                ddlIDComision.DataBind();
-
-                MateriaLogic mateL = new MateriaLogic();
-                ddlIDMateria.DataSource = mateL.GetAll();
-                ddlIDMateria.DataTextField = "Descripcion";
-                ddlIDMateria.DataValueField = "ID";
-                ddlIDMateria.DataBind();
-
-                //Selecciona la comision en DropDownList
-                //string comision = ((TextBox)e.Row.FindControl("txtComision")).Text;
-                //ddlIDComision.Items.FindByValue(comision).Selected = true;
-
-                //string materia = (e.Row.FindControl("txtDescMateria") as TextBox).Text;
-                //ddlIDMateria.Items.FindByValue(materia).Selected = true;
-            }
-        }
-
         private void LoadGrid()
         {
             CursoLogic cl = new CursoLogic();
             this.gvInscripcionesCursos.AutoGenerateColumns = false;
             this.gvInscripcionesCursos.DataSource = cl.GetAllAnioActual();
-
-
             this.gvInscripcionesCursos.DataBind();
-        }
-
-        public enum FormModes
-        {
-            Alta,
-            Baja,
-            Modificacion
-        }
-
-        public FormModes FormMode
-        {
-            get { return (FormModes)this.ViewState["FormMode"]; }
-            set { this.ViewState["FormMode"] = value; }
         }
 
         private AlumnoInscripcion Entity { get; set; }
@@ -138,143 +76,59 @@ namespace UI.Web
             this.SelectedID = (int)this.gvInscripcionesCursos.SelectedValue;
         }
 
-        //private void LoadForm(int id)
-        //{
-        //    // TODO: Ver Método
-
-        //    AlumnoInscripcion ai = 
-
-        //    //this.Entity = this.Logic.GetOne(id);
-
-        //    CursoLogic ins = new CursoLogic();
-        //    //this.gvInscripcionesCursos.DataSource = ins.GetAllAnioActual();
-
-        //    this.ddlIDCurso.SelectedValue = this.Entity.IDCurso.ToString();
-        //    //this.ddlIDMateria.SelectedValue = this.Entity.IDMateria.ToString();
-        //    //this.ddlIDComision.SelectedValue = this.Entity.ddlIDComision.ToString();
-        //    //this.txtEmail.Text = this.Entity.Email;
-        //}
-
-        protected void editarlinkButton_Click(object sender, EventArgs e)
+        private void LoadForm(int id)
         {
-            //    if (this.IsEntitySelected)
-            //    {
-            //        this.formPanel.Visible = true;
-            //        this.FormMode = FormModes.Modificacion;
-            //        this.LoadForm(this.SelectedID);
-            //    }
+            this.txtIdAlumno.Text = Session["id_persona_act"].ToString();
+            this.txtIdCurso.Text = SelectedID.ToString();
+
+            CursoLogic cl = new CursoLogic();
+            Curso cur = cl.GetOne(SelectedID);
+            this.txtDescripcionCurso.Text = cur.Descripcion;
+            
+            AlumnoLogic al = new AlumnoLogic();
+            Persona per = al.GetOne(Convert.ToInt32(Session["id_persona_act"]));
+            this.txtNombreAlumno.Text = per.Nombre+" "+per.Apellido;
+
         }
+
+        private void LoadEntity(AlumnoInscripcion inscripcion)
+        {
+            inscripcion.IDCurso = Convert.ToInt32(this.txtIdCurso.Text);
+            inscripcion.IDAlumno = Convert.ToInt32(Session["usuario_actual"]);
+            inscripcion.Condicion = " ";
+        }
+
 
         protected void lnkbtnCancelar_Click(object sender, EventArgs e)
         {
-            //    this.formPanel.Visible = false;
+            this.formPanel.Visible = false;
         }
 
         protected void lnkbtnAceptar_Click(object sender, EventArgs e)
         {
-            //    string msg;
-            //    switch (this.FormMode)
-            //    {
-            //        case FormModes.Baja:
-            //            this.DeleteEntity(this.SelectedID);
+            string msg;
+            this.Entity = new AlumnoInscripcion();
+            this.Entity.State = BusinessEntity.States.New;
+            this.LoadEntity(this.Entity);
+            this.SaveEntity(this.Entity);
 
-            //            msg = "Alumno eliminado correctamente.";
-            //            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('" + msg + "');window.location='Alumnos.aspx';", true);
+            msg = "Nueva inscripcion agregada con éxito.";
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('" + msg + "');window.location='InscripcionesCursos.aspx';", true);
 
-            //            this.LoadGrid();
-            //            break;
-            //        case FormModes.Modificacion:
-            //            this.Entity = new Persona();
-            //            this.Entity.ID = this.SelectedID;
-            //            this.Entity.State = BusinessEntity.States.Modified;
-            //            this.LoadEntity(this.Entity);
-            //            this.SaveEntity(this.Entity);
-
-            //            msg = "Alumno modificado correctamente.";
-            //            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('" + msg + "');window.location='Alumnos.aspx';", true);
-
-            //            this.LoadGrid();
-            //            break;
-            //        case FormModes.Alta:
-            //            this.Entity = new Persona();
-            //            this.Entity.State = BusinessEntity.States.New;
-            //            this.LoadEntity(this.Entity);
-            //            this.Entity.TipoPersona = Persona.TiposPersonas.Alumno;
-            //            this.SaveEntity(this.Entity);
-
-            //            msg = "Nuevo alumno agregado con éxito.";
-            //            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('" + msg + "');window.location='Alumnos.aspx';", true);
-
-            //            this.LoadGrid();
-
-            //            break;
-            //        default:
-            //            break;
-            //    }
-            //    this.formPanel.Visible = false;
+            this.formPanel.Visible = false;
         }
 
-        //private void LoadEntity(Persona persona)
-        //{
-        //    persona.Nombre = this.txtNombre.Text;
-        //    persona.Apellido = this.txtApellido.Text;
-        //    persona.Direccion = this.txtDireccion.Text;
-        //    persona.Email = this.txtEmail.Text;
-        //    persona.Legajo = int.Parse(this.txtLegajo.Text);
-        //    persona.Telefono = this.txtTelefono.Text;
-        //    persona.FechaNacimiento = Convert.ToDateTime(this.dtNacimiento.Text);
-        //    persona.IDPlan = Convert.ToInt32(this.ddlIdPlan.SelectedValue);
-        //}
-
-        //private void SaveEntity(Persona persona)
-        //{
-        //    this.Logic.Save(persona);
-        //}
-        //private void EnableForm(bool enable)
-        //{
-        //    this.txtNombre.Enabled = enable;
-        //    this.txtApellido.Enabled = enable;
-        //    this.txtDireccion.Enabled = enable;
-        //    this.txtEmail.Enabled = enable;
-        //    this.txtLegajo.Enabled = enable;
-        //    this.txtTelefono.Enabled = enable;
-        //    this.dtNacimiento.Enabled = enable;
-        //    this.ddlIdPlan.Enabled = enable;
-        //}
-
-        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        protected void inscribirlinkButton_Click(object sender, EventArgs e)
         {
-            //    if (this.IsEntitySelected)
-            //    {
-            //        this.formPanel.Visible = true;
-            //        this.FormMode = FormModes.Baja;
-            //        this.EnableForm(false);
-            //        this.LoadForm(this.SelectedID);
-            //    }
+            this.formPanel.Visible = true;
+            this.LoadForm(SelectedID);
+
         }
 
-        //private void DeleteEntity(int id)
-        //{
-        //    this.Logic.Delete(id);
-        //}
-
-        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        private void SaveEntity(AlumnoInscripcion inscripcion)
         {
-            //    this.formPanel.Visible = true;
-            //    this.FormMode = FormModes.Alta;
-            //    this.ClearForms();
-            //    this.EnableForm(true);
-            //}
-            //private void ClearForms()
-            //{
-            //    this.txtNombre.Text = string.Empty;
-            //    this.txtApellido.Text = string.Empty;
-            //    this.txtLegajo.Text = string.Empty;
-            //    this.txtEmail.Text = string.Empty;
-            //    this.txtDireccion.Text = string.Empty;
-            //    this.txtTelefono.Text = string.Empty;
-            //    this.ddlIdPlan.SelectedIndex = 0;
-            //    this.dtNacimiento.Text = string.Empty;
+            this.Logic.Save(inscripcion);
         }
+
     }
 }
